ef6c1625b3157d5d3d1e5655122d86be8a507dd8
Jardel Cruz <jardelcruz6@gmail.com>
2026-02-24 09:25:20 -0300

    reademe correto

---
+++ treinoJWT/treinoJWT/src/test/java/com/domy/treinoJWT/TreinoJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.domy.treinoJWT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreinoJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
