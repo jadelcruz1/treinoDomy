193be0113452eaea14622d8a50dd6ed43bbf70fa
Jardel Cruz <jardelcruz6@gmail.com>
2026-04-18 12:51:44 -0300

    first commit

--- treinoDomy/src/routes/PrivateRoute.tsx
+++
@@ -1,9 +0,0 @@
-import { Navigate } from "react-router-dom";
-import { useAuth } from "../context/AuthContext";
-import type { JSX } from "react";
-
-export const PrivateRoute = ({ children }: { children: JSX.Element }) => {
-  const { isAuthenticated } = useAuth();
-
-  return isAuthenticated ? children : <Navigate to="/" />;
-};
